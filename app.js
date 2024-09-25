a182b71814cee52d4826ea69784a29932c8a68d7
Avadhesh Kumar <159410471+AvadheshVns@users.noreply.github.com>
2024-09-25 09:54:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 const parent=React.createElement("div", {id:"parent"},
     React.createElement("div", {id:"child"},
-    React.createElement("h1", {},"hello shit")));
+    React.createElement("h1", {},"hello bhai")));
 console.log(parent);
 const root=ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
