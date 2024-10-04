9291ad2116f1f0ab77605b26e67f3f810bb536d4
Avadhesh_Kumar <avadheshkumar273@gmail.com>
2024-10-05 02:20:58 +0530

    initiated app

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent=React.createElement("div", {id:"parent"},
     React.createElement("div", {id:"child"},
-    React.createElement("h1", {},"hello bhai")));
+    React.createElement("h1", {},"Welcome to react application")));
 console.log(parent);
 const root=ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
