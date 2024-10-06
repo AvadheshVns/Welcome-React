79d46584a560d6b59209c455e4828f32147c1fa0
Avadhesh_Kumar <avadheshkumar273@gmail.com>
2024-10-07 01:01:07 +0530

    functional component

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+ import React from "react";
+ import ReactDOM from "react-dom/client";
 
-const parent=React.createElement("div", {id:"parent"},
-    React.createElement("div", {id:"child"},
-    React.createElement("h1", {},"Welcome to react application")));
-console.log(parent);
+// const parent=React.createElement("div", {id:"parent"},
+//     React.createElement("div", {id:"child"},
+//     React.createElement("h1", {},"Welcome to react application🌈 ")));
+// console.log(parent);
+//const root=ReactDOM.createRoot(document.getElementById("root"));
+// root.render(parent);
+
 const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+
+ //React Element
+ const Handling = (
+ <h1>Welcome to react application🌈 
+
+ </h1>);
+
+
+//React component
+const HeadingComponent=() =>(
+    
+   <div  id="container">
+    {Handling}
+     <h1 className="abc">Good morning!</h1>
+
+    </div>
+);
+
+root.render(<HeadingComponent />);
+//root.render(handling);
